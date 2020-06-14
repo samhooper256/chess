7c2a1bb48df8df2a9ef9708226d3480e79683977
samhooper256 <samhooper256@gmail.com>
2020-06-13 20:19:30 -0500

    Fixed Bug

--- chess/src/chess/piecebuilder/DropPathPane.java
+++ chess/src/chess/piecebuilder/DropPathPane.java
@@ -5,8 +5,11 @@ import java.util.Set;
 import chess.util.InputVerification;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
+import javafx.geometry.Side;
 import javafx.scene.Node;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.BorderPane;
@@ -14,37 +17,42 @@ import javafx.scene.layout.BorderPane;
 public class DropPathPane extends BorderPane implements InputVerification {
 	private Label label;
 	private String pathType;
-	private boolean pathHasBeenDropped;
-	public DropPathPane(boolean addFinisher) {
+	private boolean pathHasBeenDropped, addFinisher;
+	public DropPathPane(boolean initAddFinisher) {
 		super();
+		this.addFinisher = initAddFinisher;
+		ContextMenu cm = new ContextMenu();
+		MenuItem booleanPath = new MenuItem("Boolean Path");
+		booleanPath.setOnAction(actionEvent -> {
+			drop(PathBuilder.BOOLEAN_BUILDER);
+		});
+		MenuItem integerPath = new MenuItem("Integer Path");
+		integerPath.setOnAction(actionEvent -> {
+			drop(PathBuilder.INTEGER_BUILDER);
+		});
+		MenuItem anythingPath = new MenuItem("Anything Path");
+		anythingPath.setOnAction(actionEvent -> {
+			drop(PathBuilder.OBJECT_BUILDER);
+		});
+		cm.getItems().addAll(booleanPath, integerPath, anythingPath);
+		
 		this.pathHasBeenDropped = false;
-		this.label = new Label("Drop Any Path");
+		this.label = new Label("Drop Path or Right Click");
 		this.pathType = null;
 		this.getStyleClass().add("drop-path-pane");
 		label.setPadding(new Insets(5));
 		this.setCenter(label);
+		this.setOnContextMenuRequested(contextMenuEvent -> {
+			cm.show(this, contextMenuEvent.getScreenX(), contextMenuEvent.getScreenY());
+		});
 		this.setOnDragDropped(dragEvent -> {
 			Dragboard db = dragEvent.getDragboard();
 	        boolean success = false;
 	        if (db.hasString()) {
 	        	Set<TransferMode> transferModes = db.getTransferModes();
 	        	if(transferModes.size() == 1 && transferModes.iterator().next() == TransferMode.COPY) {
-	    			String builderName = db.getString();
-	                PathBuilder builder = PathBuilder.getBuilderByStringNull(builderName);
-	                if(builder == null) {
-	                	System.out.println("It's null OOPS");
-	                	dragEvent.setDropCompleted(false);
-	                	return; //don't consume so it goes to MultiCondition
-	                }
-	                else {
-	                	CustomConditionBox ccb = ((CustomConditionBox) getParent());
-	                	ObservableList<Node> children = ccb.getChildren();
-	                	int myIndex = children.indexOf(DropPathPane.this);
-	                	DropPathPane.this.pathHasBeenDropped = true;
-	                	ConditionBox.clearPast(children, myIndex, true);
-	                	ccb.addBuilder(builderName, addFinisher);
-	                	success = true;
-	                }
+	    			drop(db.getString());
+                	success = true;
 	        	}
 	        }
 	        dragEvent.setDropCompleted(success);
@@ -58,6 +66,18 @@ public class DropPathPane extends BorderPane implements InputVerification {
 	        							//if it's unsuccessful here.
 	        }
 		});
+	}
+	
+	private void drop(String builderName) {
+		if(pathHasBeenDropped) {
+			return;
+		}
+    	CustomConditionBox ccb = ((CustomConditionBox) getParent());
+    	ObservableList<Node> children = ccb.getChildren();
+    	int myIndex = children.indexOf(DropPathPane.this);
+    	DropPathPane.this.pathHasBeenDropped = true;
+    	ConditionBox.clearPast(children, myIndex, true);
+    	ccb.addBuilder(builderName, addFinisher);
 	}
 
 	@Override
